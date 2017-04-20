198c3cefc1a07c7744b878db163c7b31fc3860a5
up201503708 <up201503708@B03AZ0508.salas.feupsig.fe.up.pt>
2017-04-20 16:32:38 +0100

    game working

--- Pong_notAndroid/src/gui/GraphicsAndListeners.java
+++ Pong_notAndroid/src/gui/GraphicsAndListeners.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -11,15 +13,17 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import logic.Game;
 
-public class GraphicsAndListeners extends JPanel implements KeyListener, MouseListener{
+public class GraphicsAndListeners extends JPanel implements KeyListener, MouseListener, ActionListener{
 	
 	private BufferedImage square;
 	private BufferedImage ball;
 	private Game game;
 	GraphicInterface graphics;
+	Timer t;
 	
 	public GraphicsAndListeners(GraphicInterface g){
 		addKeyListener(this);
@@ -34,16 +38,20 @@ public class GraphicsAndListeners extends JPanel implements KeyListener, MouseLi
 		game=new Game();
 		this.graphics=g;
 		
+		t=new Timer(20,this);
+		
 	}
 	
 	
 	@Override
 	protected void paintComponent(Graphics g) {
-	super.paintComponent(g);
-	g.drawImage(ball, (int)game.getBall().getPosition()[0]-12, (int)game.getBall().getPosition()[1]-12, 24, 24, null);
+	 super.paintComponent(g);
+	g.drawImage(ball, (int)game.getBall().getPosition()[0]-game.ballSize/2, (int)game.getBall().getPosition()[1]-game.ballSize/2, game.ballSize, game.ballSize, null);
+	g.drawImage(square,(int)game.getPlayer1pos()[0]-game.paddleSize[0]/2,(int)game.getPlayer1pos()[1]-game.paddleSize[1]/2, game.paddleSize[0],game.paddleSize[1],null);
+	g.drawImage(square,(int)game.getPlayer2pos()[0]-game.paddleSize[0]/2,(int)game.getPlayer2pos()[1]-game.paddleSize[1]/2, game.paddleSize[0],game.paddleSize[1],null);
 	
 	//g.fillOval((int)(game.getBall().getPosition()[0]-12), (int)(game.getBall().getPosition()[1]-12), 24, 24);
-	System.out.println("painted");
+	//System.out.println("painted");
 	}
 
 	@Override
@@ -54,22 +62,48 @@ public class GraphicsAndListeners extends JPanel implements KeyListener, MouseLi
 			break; 
 		case KeyEvent.VK_RIGHT:
 			game.setPlaying(true); 
-			gameInProgress();
-			//game.getBall().setPositionx(game.getBall().getPosition()[0]+10);
-			//System.out.println("pressed");
-			
+			t.start();
 		break;  
-		case KeyEvent.VK_UP: break; 
-		case KeyEvent.VK_DOWN: break; 
+		case KeyEvent.VK_UP:
+			game.p1Up=true;
+			//System.out.println("UP, pressed");
+			
+		break; 
+		case KeyEvent.VK_DOWN:
+			game.p1Down=true;
+			//System.out.println("Down, pressed");
+		break; 
+		case KeyEvent.VK_W:
+			game.p2Up=true;
+			break;
+        case KeyEvent.VK_S:
+        	game.p2Down=true;
+			break;	
+        case KeyEvent.VK_R:
+        	game.reset();
+        	repaint();
+			break;	
 		}
 		
-		//repaint();
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		switch(arg0.getKeyCode()){
+		case KeyEvent.VK_UP:
+			game.p1Up=false;
+			break; 
+		case KeyEvent.VK_DOWN:
+			game.p1Down=false;
+			break; 
+		case KeyEvent.VK_W:
+			game.p2Up=false;
+			break;
+		case KeyEvent.VK_S:
+			game.p2Down=false;
+			break;	
+		}
+
 	}
 
 	@Override
@@ -78,22 +112,6 @@ public class GraphicsAndListeners extends JPanel implements KeyListener, MouseLi
 		
 	}
 	
-	public void gameInProgress(){
-		
-		//while(game.getPlaying()){
-			game.update();
-			repaint();
-			System.out.println("pressed");
-			System.out.println((int)game.getBall().getPosition()[0] +" "+(int)game.getBall().getPosition()[1]);
-			
-			/*try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-		//}
-	}
 
 
 	@Override
@@ -127,6 +145,17 @@ public class GraphicsAndListeners extends JPanel implements KeyListener, MouseLi
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if(game.getPlaying()){
+		game.update();
+		repaint();
+		//System.out.println((int)game.getBall().getPosition()[0] +" "+(int)game.getBall().getPosition()[1]);
+	}
 		
 	}
 
--- Pong_notAndroid/src/logic/Game.java
+++ Pong_notAndroid/src/logic/Game.java
@@ -2,20 +2,64 @@ package logic;
 
 public class Game {
 	private Ball ball;
-	private int player1pos[];
-	private int player2pos[];
-	boolean playing;
+	private double player1pos[];
+	private double player2pos[];
+	public boolean p1Up,p1Down;
+	public boolean p2Up,p2Down;
+	private double ballVelocity;
+	private double paddleVelocity;
+	public static final int ballSize=24;
+	public static final int paddleSize[]={10,90};
+	private boolean playing;
+	
+	private double startingPos[];
 	
 	public Game(){
 		ball=new Ball();
+		
+		player1pos=new double[2];
+		player1pos[0]=724;
+		player1pos[1]=150;
+		
+		player2pos=new double[2];
+		player2pos[0]=10;
+		player2pos[1]=150;
+	
 		playing=false;
+		p1Up=false;
+		p1Down=false;
+		p2Up=false;
+		p2Down=false;
+		
+		ballVelocity=10;
+		paddleVelocity=5;
+		
+		startingPos=new double[]{player1pos[0],player1pos[1],player2pos[0],player2pos[1],ball.getPosition()[0],ball.getPosition()[1]};
+		
 	}
 	
 	public void update(){
 		collisionHandler();
 		
-		ball.setPositionx(ball.getPosition()[0]+10*ball.getVector()[0]);
-		ball.setPositiony(ball.getPosition()[1]+10*ball.getVector()[1]);
+		if(playing){
+			
+		if(p1Up==true && player1pos[1]>=0+paddleSize[1]/2){
+			player1pos[1]=player1pos[1]-paddleVelocity;
+		}
+		else if(p1Down==true && player1pos[1]<=412-paddleSize[1]/2){
+			player1pos[1]=player1pos[1]+paddleVelocity;
+		}
+		
+		if(p2Up==true && player2pos[1]>=0+paddleSize[1]/2){
+			player2pos[1]=player2pos[1]-paddleVelocity;
+		}
+		else if(p2Down==true && player2pos[1]<=412-paddleSize[1]/2){
+			player2pos[1]=player2pos[1]+paddleVelocity;
+		}
+		
+		ball.setPositionx(ball.getPosition()[0]+ballVelocity*ball.getVector()[0]);
+		ball.setPositiony(ball.getPosition()[1]+ballVelocity*ball.getVector()[1]);
+		}
 		
 	}
 	
@@ -34,20 +78,56 @@ public class Game {
 	
 	public void collisionHandler(){
 		
-		if(ball.getPosition()[0]<=12 && ball.getVector()[0]<0){
+		boolean collidedWithPaddle=false;
+		
+		if(ball.getPosition()[0]<=ballSize/2+player2pos[0]+paddleSize[0]/2 && ball.getPosition()[1]<= player2pos[1]+paddleSize[1]/2 && ball.getPosition()[1]>= player2pos[1]-paddleSize[1]/2 && ball.getVector()[0]<0){
+			
+			collidedWithPaddle=true;
 			ball.setVector_x(-ball.getVector()[0]);
+		}
+		
+        if(ball.getPosition()[0]>=player1pos[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<= player1pos[1]+paddleSize[1]/2 && ball.getPosition()[1]>= player1pos[1]-paddleSize[1]/2 && ball.getVector()[0]>0){
+        	collidedWithPaddle=true;
+        	ball.setVector_x(-ball.getVector()[0]);
+			
+		}
+		
+		
+		if(!collidedWithPaddle){
+			
+		if(ball.getPosition()[0]<=12 && ball.getVector()[0]<0){
+			playing=false;
 		}
 		else if(ball.getPosition()[1]<=12 && ball.getVector()[1]<0){
 			ball.setVector_y(-ball.getVector()[1]);
 		}
 		else if(ball.getPosition()[0]>=734-12 && ball.getVector()[0]>0){
-			ball.setVector_x(-ball.getVector()[0]);
+			playing=false;
 		}
 		else if(ball.getPosition()[1]>=412-12 && ball.getVector()[1]>0){
 			ball.setVector_y(-ball.getVector()[1]);
 			
 		}
 		
+		}
+			
+		
 	}
 
+	public double[] getPlayer1pos() {
+		return player1pos;
+	}
+
+	public double[] getPlayer2pos() {
+		return player2pos;
+	}
+	
+	public void reset(){
+		player1pos[0]=startingPos[0];
+		player1pos[1]=startingPos[1];
+		player2pos[0]=startingPos[2];
+		player2pos[1]=startingPos[3];
+		ball.setPositionx(startingPos[4]);
+		ball.setPositiony(startingPos[5]);
+	}
 }
