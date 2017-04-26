3ee30ca8f772b6cc8fb6b63b8d39ddb352ca702c
Leonardo <Leonardo@Leonardo-PC>
2017-04-26 18:37:01 +0100

    Fixed Collisions

--- Pong_notAndroid/src/logic/Game.java
+++ Pong_notAndroid/src/logic/Game.java
@@ -9,6 +9,9 @@ public class Game {
 	private double paddleVelocity;
 	public static final int ballSize=24;
 	public static final int paddleSize[]={10,90};
+	public static final int screen_x=734;
+	public static final int screen_y=412;
+	
 	private boolean playing;
 	
 	private double startingPos[];
@@ -16,12 +19,12 @@ public class Game {
 	public Game(){
 		ball=new Ball(500,150,0);
 		
-		p1=new Player(724,150);
-		p2=new Player(10,150);
+		p1=new Player(screen_x-10,screen_y/2);
+		p2=new Player(10,screen_y/2);
 	
 		playing=false;
 		
-		ballVelocity=7;
+		ballVelocity=5;
 		paddleVelocity=4;
 		
 		o=new Obstacle[5];
@@ -73,136 +76,229 @@ public class Game {
 		playing=a;
 		
 	}
-	
+
 	public void collisionHandler(){
-		
+
 		boolean collidedWithPaddle=false;
-		
+
 		/*if(ball.getPosition()[0]<=ballSize/2+player2pos[0]+paddleSize[0]/2 && ball.getPosition()[1]<= player2pos[1]+paddleSize[1]/2 && ball.getPosition()[1]>= player2pos[1]-paddleSize[1]/2 && ball.getVector()[0]<0){
-			
+
 			collidedWithPaddle=true;
 			ball.setVector_x(-ball.getVector()[0]);
 		}
-		
+
         if(ball.getPosition()[0]>=player1pos[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<= player1pos[1]+paddleSize[1]/2 && ball.getPosition()[1]>= player1pos[1]-paddleSize[1]/2 && ball.getVector()[0]>0){
         	collidedWithPaddle=true;
         	ball.setVector_x(-ball.getVector()[0]);
-			
+
 		}*/
-        
+
 		//Player 2 Paddle
 
-        if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2 && ball.getPosition()[1]> p2.getPos()[1]+paddleSize[1]/2-paddleSize[1]/7 && ball.getVector()[0]<0){
+		if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 &&
+				ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2 && 
+				ball.getPosition()[1]> p2.getPos()[1]+paddleSize[1]/2-paddleSize[1]/7 &&
+				ball.getVector()[0]<0 &&
+				ball.getPosition()[0]>=p2.getPos()[0]-paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(Math.cos(Math.toRadians(60)));
-        	ball.setVector_y(Math.sin(Math.toRadians(60)));
-        }else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && ball.getPosition()[1]<=p2.getPos()[1]+paddleSize[1]/2-paddleSize[1]/7 && ball.getPosition()[1]> p2.getPos()[1]+paddleSize[1]/2-2*paddleSize[1]/7 && ball.getVector()[0]<0){
+			collidedWithPaddle=true;
+			ball.setVector_x(Math.cos(Math.toRadians(60)));
+			ball.setVector_y(Math.sin(Math.toRadians(60)));
+		}else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 &&
+				ball.getPosition()[1]<=p2.getPos()[1]+paddleSize[1]/2-paddleSize[1]/7 && 
+				ball.getPosition()[1]> p2.getPos()[1]+paddleSize[1]/2-2*paddleSize[1]/7 &&
+				ball.getVector()[0]<0 &&
+				ball.getPosition()[0]>=p2.getPos()[0]-paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(Math.cos(Math.toRadians(40)));
-        	ball.setVector_y(Math.sin(Math.toRadians(40)));
-        }else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-2*paddleSize[1]/7 && ball.getPosition()[1]>  p2.getPos()[1]+paddleSize[1]/2-3*paddleSize[1]/7 && ball.getVector()[0]<0){
+			collidedWithPaddle=true;
+			ball.setVector_x(Math.cos(Math.toRadians(40)));
+			ball.setVector_y(Math.sin(Math.toRadians(40)));
+		}else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 &&
+				ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-2*paddleSize[1]/7 && 
+				ball.getPosition()[1]>  p2.getPos()[1]+paddleSize[1]/2-3*paddleSize[1]/7 && 
+				ball.getVector()[0]<0 &&
+				ball.getPosition()[0]>=p2.getPos()[0]-paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(Math.cos(Math.toRadians(20)));
-        	ball.setVector_y(Math.sin(Math.toRadians(20)));
-        }else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-3*paddleSize[1]/7 && ball.getPosition()[1]>  p2.getPos()[1]+paddleSize[1]/2-4*paddleSize[1]/7 && ball.getVector()[0]<0){
+			collidedWithPaddle=true;
+			ball.setVector_x(Math.cos(Math.toRadians(20)));
+			ball.setVector_y(Math.sin(Math.toRadians(20)));
+		}else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 &&
+				ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-3*paddleSize[1]/7 &&
+				ball.getPosition()[1]>  p2.getPos()[1]+paddleSize[1]/2-4*paddleSize[1]/7 && 
+				ball.getVector()[0]<0 &&
+				ball.getPosition()[0]>=p2.getPos()[0]-paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(Math.cos(Math.toRadians(0)));
-        	ball.setVector_y(Math.sin(Math.toRadians(0)));
-        }else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-4*paddleSize[1]/7 && ball.getPosition()[1]>  p2.getPos()[1]+paddleSize[1]/2-5*paddleSize[1]/7 && ball.getVector()[0]<0){
+			collidedWithPaddle=true;
+			ball.setVector_x(Math.cos(Math.toRadians(0)));
+			ball.setVector_y(Math.sin(Math.toRadians(0)));
+		}else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 &&
+				ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-4*paddleSize[1]/7 && 
+				ball.getPosition()[1]>  p2.getPos()[1]+paddleSize[1]/2-5*paddleSize[1]/7 &&
+				ball.getVector()[0]<0 &&
+				ball.getPosition()[0]>=p2.getPos()[0]-paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(Math.cos(Math.toRadians(20)));
-        	ball.setVector_y(-Math.sin(Math.toRadians(20)));
-        }else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-5*paddleSize[1]/7 && ball.getPosition()[1]>  p2.getPos()[1]+paddleSize[1]/2-6*paddleSize[1]/7 && ball.getVector()[0]<0){
+			collidedWithPaddle=true;
+			ball.setVector_x(Math.cos(Math.toRadians(20)));
+			ball.setVector_y(-Math.sin(Math.toRadians(20)));
+		}else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && 
+				ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-5*paddleSize[1]/7 &&
+				ball.getPosition()[1]>  p2.getPos()[1]+paddleSize[1]/2-6*paddleSize[1]/7 && 
+				ball.getVector()[0]<0 &&
+				ball.getPosition()[0]>=p2.getPos()[0]-paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(Math.cos(Math.toRadians(40)));
-        	ball.setVector_y(-Math.sin(Math.toRadians(40)));
-        }else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-6*paddleSize[1]/7 && ball.getPosition()[1]>=  p2.getPos()[1]+paddleSize[1]/2-7*paddleSize[1]/7 && ball.getVector()[0]<0){
+			collidedWithPaddle=true;
+			ball.setVector_x(Math.cos(Math.toRadians(40)));
+			ball.setVector_y(-Math.sin(Math.toRadians(40)));
+		}else if(ball.getPosition()[0]<=ballSize/2+p2.getPos()[0]+paddleSize[0]/2 && 
+				ball.getPosition()[1]<= p2.getPos()[1]+paddleSize[1]/2-6*paddleSize[1]/7 && 
+				ball.getPosition()[1]>=  p2.getPos()[1]+paddleSize[1]/2-7*paddleSize[1]/7 &&
+				ball.getVector()[0]<0 &&
+				ball.getPosition()[0]>=p2.getPos()[0]-paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(Math.cos(Math.toRadians(60)));
-        	ball.setVector_y(-Math.sin(Math.toRadians(60)));
-        }
-        
-        //Player 1 Paddle
-        
-        if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2 && ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-paddleSize[1]/7 && ball.getVector()[0]>0){
+			collidedWithPaddle=true;
+			ball.setVector_x(Math.cos(Math.toRadians(60)));
+			ball.setVector_y(-Math.sin(Math.toRadians(60)));
+		}
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(-Math.cos(Math.toRadians(60)));
-        	ball.setVector_y(Math.sin(Math.toRadians(60)));
-        }else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<=p1.getPos()[1]+paddleSize[1]/2-paddleSize[1]/7 && ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-2*paddleSize[1]/7 && ball.getVector()[0]>0){
+		//Player 1 Paddle
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(-Math.cos(Math.toRadians(40)));
-        	ball.setVector_y(Math.sin(Math.toRadians(40)));
-        }else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-2*paddleSize[1]/7 && ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-3*paddleSize[1]/7 && ball.getVector()[0]>0){
+		if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 &&
+				ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2 &&
+				ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-paddleSize[1]/7 &&
+				ball.getVector()[0]>0 && 
+				ball.getPosition()[0]<=p1.getPos()[0]+paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(-Math.cos(Math.toRadians(20)));
-        	ball.setVector_y(Math.sin(Math.toRadians(20)));
-        }else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-3*paddleSize[1]/7 && ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-4*paddleSize[1]/7 && ball.getVector()[0]>0){
+			collidedWithPaddle=true;
+			ball.setVector_x(-Math.cos(Math.toRadians(60)));
+			ball.setVector_y(Math.sin(Math.toRadians(60)));
+		}else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && 
+				ball.getPosition()[1]<=p1.getPos()[1]+paddleSize[1]/2-paddleSize[1]/7 && 
+				ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-2*paddleSize[1]/7 && 
+				ball.getVector()[0]>0 &&
+				ball.getPosition()[0]<=p1.getPos()[0]+paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(-Math.cos(Math.toRadians(0)));
-        	ball.setVector_y(Math.sin(Math.toRadians(0)));
-        }else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-4*paddleSize[1]/7 && ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-5*paddleSize[1]/7 && ball.getVector()[0]>0){
+			collidedWithPaddle=true;
+			ball.setVector_x(-Math.cos(Math.toRadians(40)));
+			ball.setVector_y(Math.sin(Math.toRadians(40)));
+		}else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && 
+				ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-2*paddleSize[1]/7 &&
+				ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-3*paddleSize[1]/7 &&
+				ball.getVector()[0]>0 && 
+				ball.getPosition()[0]<=p1.getPos()[0]+paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(-Math.cos(Math.toRadians(20)));
-        	ball.setVector_y(-Math.sin(Math.toRadians(20)));
-        }else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-5*paddleSize[1]/7 && ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-6*paddleSize[1]/7 && ball.getVector()[0]>0){
+			collidedWithPaddle=true;
+			ball.setVector_x(-Math.cos(Math.toRadians(20)));
+			ball.setVector_y(Math.sin(Math.toRadians(20)));
+		}else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && 
+				ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-3*paddleSize[1]/7 && 
+				ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-4*paddleSize[1]/7 &&
+				ball.getVector()[0]>0 && 
+				ball.getPosition()[0]<=p1.getPos()[0]+paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(-Math.cos(Math.toRadians(40)));
-        	ball.setVector_y(-Math.sin(Math.toRadians(40)));
-        }else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-6*paddleSize[1]/7 && ball.getPosition()[1]>= p1.getPos()[1]+paddleSize[1]/2-7*paddleSize[1]/7 && ball.getVector()[0]>0){
+			collidedWithPaddle=true;
+			ball.setVector_x(-Math.cos(Math.toRadians(0)));
+			ball.setVector_y(Math.sin(Math.toRadians(0)));
+		}else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 &&
+				ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-4*paddleSize[1]/7 && 
+				ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-5*paddleSize[1]/7 &&
+				ball.getVector()[0]>0 &&
+				ball.getPosition()[0]<=p1.getPos()[0]+paddleSize[0]/2){
 
-        	collidedWithPaddle=true;
-        	ball.setVector_x(-Math.cos(Math.toRadians(60)));
-        	ball.setVector_y(-Math.sin(Math.toRadians(60)));
-        }
-        
-        
-		
-		
+			collidedWithPaddle=true;
+			ball.setVector_x(-Math.cos(Math.toRadians(20)));
+			ball.setVector_y(-Math.sin(Math.toRadians(20)));
+		}else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 &&
+				ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-5*paddleSize[1]/7 && 
+				ball.getPosition()[1]> p1.getPos()[1]+paddleSize[1]/2-6*paddleSize[1]/7 &&
+				ball.getVector()[0]>0 && 
+				ball.getPosition()[0]<=p1.getPos()[0]+paddleSize[0]/2){
+
+			collidedWithPaddle=true;
+			ball.setVector_x(-Math.cos(Math.toRadians(40)));
+			ball.setVector_y(-Math.sin(Math.toRadians(40)));
+		}else if(ball.getPosition()[0]>=p1.getPos()[0]-paddleSize[0]/2-ballSize/2 && 
+				ball.getPosition()[1]<= p1.getPos()[1]+paddleSize[1]/2-6*paddleSize[1]/7 &&
+				ball.getPosition()[1]>= p1.getPos()[1]+paddleSize[1]/2-7*paddleSize[1]/7 && 
+				ball.getVector()[0]>0 && 
+				ball.getPosition()[0]<=p1.getPos()[0]+paddleSize[0]/2){
+
+			collidedWithPaddle=true;
+			ball.setVector_x(-Math.cos(Math.toRadians(60)));
+			ball.setVector_y(-Math.sin(Math.toRadians(60)));
+		}
+
+
+
+
 		if(!collidedWithPaddle){
-			
-		if(ball.getPosition()[0]<=ballSize/2 && ball.getVector()[0]<0){
-			playing=false;
-		}
-		else if(ball.getPosition()[1]<=ballSize/2 && ball.getVector()[1]<0){
-			ball.setVector_y(-ball.getVector()[1]);
-		}
-		else if(ball.getPosition()[0]>=734-ballSize/2 && ball.getVector()[0]>0){
-			playing=false;
-		}
-		else if(ball.getPosition()[1]>=412-ballSize/2 && ball.getVector()[1]>0){
-			ball.setVector_y(-ball.getVector()[1]);
-			
-		}
-		
-		for(int i=0;i< o.length;i++){
 
-			if(ball.getVector()[1]>0 && ball.getPosition()[1]<o[i].getK()[0]*ball.getPosition()[0]+o[i].getB()[0] && ball.getPosition()[1]<o[i].getK()[1]*ball.getPosition()[0]+o[i].getB()[1] && ball.getPosition()[1]>o[i].getPos()[1]-o[i].getSize()[1]/2-ballSize/2 && ball.getPosition()[0]<o[i].getPos()[0]+o[i].getSize()[0]/2+ballSize/4 && ball.getPosition()[0]>o[i].getPos()[0]-o[i].getSize()[0]/2-ballSize/4){
+			if(ball.getPosition()[0]<=ballSize/2 && ball.getVector()[0]<0){
+				playing=false;
+			}
+			else if(ball.getPosition()[1]<=ballSize/2 && ball.getVector()[1]<0){
 				ball.setVector_y(-ball.getVector()[1]);
 			}
-			else if(ball.getVector()[0]<0 && ball.getPosition()[1]<o[i].getK()[0]*ball.getPosition()[0]+o[i].getB()[0] && ball.getPosition()[1]>o[i].getK()[1]*ball.getPosition()[0]+o[i].getB()[1] && ball.getPosition()[0]<o[i].getPos()[0]+o[i].getSize()[0]/2+ballSize/2 && ball.getPosition()[1]<o[i].getPos()[1]+o[i].getSize()[1]/2+ballSize/4 && ball.getPosition()[1]>o[i].getPos()[1]-o[i].getSize()[1]/2-ballSize/4){
-				ball.setVector_x(-ball.getVector()[0]);
+			else if(ball.getPosition()[0]>=screen_x-ballSize/2 && ball.getVector()[0]>0){
+				playing=false;
 			}
-			else if(ball.getVector()[1]<0 && ball.getPosition()[1]>o[i].getK()[0]*ball.getPosition()[0]+o[i].getB()[0] && ball.getPosition()[1]>o[i].getK()[1]*ball.getPosition()[0]+o[i].getB()[1] && ball.getPosition()[1]<o[i].getPos()[1]+o[i].getSize()[1]/2+ballSize/2 && ball.getPosition()[0]<o[i].getPos()[0]+o[i].getSize()[0]/2+ballSize/4 && ball.getPosition()[0]>o[i].getPos()[0]-o[i].getSize()[0]/2-ballSize/4){
+			else if(ball.getPosition()[1]>=screen_y-ballSize/2 && ball.getVector()[1]>0){
 				ball.setVector_y(-ball.getVector()[1]);
-			}
-			else if(ball.getVector()[0]>0 && ball.getPosition()[1]>o[i].getK()[0]*ball.getPosition()[0]+o[i].getB()[0] && ball.getPosition()[1]<o[i].getK()[1]*ball.getPosition()[0]+o[i].getB()[1] && ball.getPosition()[0]>o[i].getPos()[0]-o[i].getSize()[0]/2-ballSize/2 && ball.getPosition()[1]<o[i].getPos()[1]+o[i].getSize()[1]/2+ballSize/4 && ball.getPosition()[1]>o[i].getPos()[1]-o[i].getSize()[1]/2-ballSize/4){
-				ball.setVector_x(-ball.getVector()[0]);
+
 			}
 
-		}
+			for(int i=0;i< o.length;i++){
+
+				if(ball.getVector()[1]>0 &&
+						ball.getPosition()[1]<o[i].getK()[0]*ball.getPosition()[0]+o[i].getB()[0] &&
+						ball.getPosition()[1]<o[i].getK()[1]*ball.getPosition()[0]+o[i].getB()[1] &&
+						ball.getPosition()[1]>o[i].getPos()[1]-o[i].getSize()[1]/2-ballSize/2 &&
+						ball.getPosition()[0]<o[i].getPos()[0]+o[i].getSize()[0]/2 && 
+						ball.getPosition()[0]>o[i].getPos()[0]-o[i].getSize()[0]/2){
+					ball.setVector_y(-ball.getVector()[1]);
+				}
+				else if(ball.getVector()[0]<0 && 
+						ball.getPosition()[1]<o[i].getK()[0]*ball.getPosition()[0]+o[i].getB()[0] &&
+						ball.getPosition()[1]>o[i].getK()[1]*ball.getPosition()[0]+o[i].getB()[1] && 
+						ball.getPosition()[0]<o[i].getPos()[0]+o[i].getSize()[0]/2+ballSize/2 && 
+						ball.getPosition()[1]<o[i].getPos()[1]+o[i].getSize()[1]/2 && 
+						ball.getPosition()[1]>o[i].getPos()[1]-o[i].getSize()[1]/2){
+					ball.setVector_x(-ball.getVector()[0]);
+				}
+				else if(ball.getVector()[1]<0 && 
+						ball.getPosition()[1]>o[i].getK()[0]*ball.getPosition()[0]+o[i].getB()[0] && 
+						ball.getPosition()[1]>o[i].getK()[1]*ball.getPosition()[0]+o[i].getB()[1] && 
+						ball.getPosition()[1]<o[i].getPos()[1]+o[i].getSize()[1]/2+ballSize/2 &&
+						ball.getPosition()[0]<o[i].getPos()[0]+o[i].getSize()[0]/2 && 
+						ball.getPosition()[0]>o[i].getPos()[0]-o[i].getSize()[0]/2){
+					ball.setVector_y(-ball.getVector()[1]);
+				}
+				else if(ball.getVector()[0]>0 &&
+						ball.getPosition()[1]>o[i].getK()[0]*ball.getPosition()[0]+o[i].getB()[0] && 
+						ball.getPosition()[1]<o[i].getK()[1]*ball.getPosition()[0]+o[i].getB()[1] && 
+						ball.getPosition()[0]>o[i].getPos()[0]-o[i].getSize()[0]/2-ballSize/2 && 
+						ball.getPosition()[1]<o[i].getPos()[1]+o[i].getSize()[1]/2 &&
+						ball.getPosition()[1]>o[i].getPos()[1]-o[i].getSize()[1]/2){
+					ball.setVector_x(-ball.getVector()[0]);
+				}
+				else if(Math.sqrt((Math.pow(((o[i].getPos()[0]-o[i].getSize()[0]/2)-ball.getPosition()[0]),2)+Math.pow(((o[i].getPos()[1]-o[i].getSize()[1]/2)-ball.getPosition()[1]),2)))<= ballSize/2 && !(ball.getVector()[0]<0 && ball.getVector()[1]<0)){
+					ball.setVector_x(-Math.cos(Math.toRadians(45)));
+					ball.setVector_y(-Math.sin(Math.toRadians(45)));
+
+				}
+				else if(Math.sqrt((Math.pow(((o[i].getPos()[0]+o[i].getSize()[0]/2)-ball.getPosition()[0]),2)+Math.pow(((o[i].getPos()[1]-o[i].getSize()[1]/2)-ball.getPosition()[1]),2)))<= ballSize/2 && !(ball.getVector()[0]>0 && ball.getVector()[1]<0)){
+					ball.setVector_x(Math.cos(Math.toRadians(45)));
+					ball.setVector_y(-Math.sin(Math.toRadians(45)));
+				}
+				else if(Math.sqrt((Math.pow(((o[i].getPos()[0]+o[i].getSize()[0]/2)-ball.getPosition()[0]),2)+Math.pow(((o[i].getPos()[1]+o[i].getSize()[1]/2)-ball.getPosition()[1]),2)))<= ballSize/2 && !(ball.getVector()[0]>0 && ball.getVector()[1]>0)){
+					ball.setVector_x(Math.cos(Math.toRadians(45)));
+					ball.setVector_y(Math.sin(Math.toRadians(45)));
+				}
+				else if(Math.sqrt((Math.pow(((o[i].getPos()[0]-o[i].getSize()[0]/2)-ball.getPosition()[0]),2)+Math.pow(((o[i].getPos()[1]+o[i].getSize()[1]/2)-ball.getPosition()[1]),2)))<= ballSize/2 && !(ball.getVector()[0]<0 && ball.getVector()[1]>0)){
+					ball.setVector_x(-Math.cos(Math.toRadians(45)));
+					ball.setVector_y(Math.sin(Math.toRadians(45)));
+				}
+
+			}
 		}
 
 	}
